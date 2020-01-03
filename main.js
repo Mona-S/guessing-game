cde40ca6b9abf0822e7b1b09c8dff3e94576efc1
Mona-S <forward2cm@gmail.com>
2020-01-03 06:06:45 +0000

    added scores

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(document).ready(initializeApp); // This line is defining a function that will run once the HTML document loads.
+$(document).ready(initializeApp); 
 
 var the_number = null;
 var name = null;
@@ -6,34 +6,24 @@ var score = 1;
 
 function pick_number(){
     var random_number = Math.floor(Math.random()*11);
-    console.log('random',random_number);
     return random_number;
 }
+
 function initializeApp () {
     the_number = pick_number();
     $("#submit").click(make_guess);
     $("#startAgain").click(reset_game);
     
-    
-   
-//this is where you need to place the function call for your random number generator function.
-//You will also place your clickhandler in here
-}
-function reset_game(){
-    //set text in response to nothing
-    the_number = pick_number();
-    console.log('random',the_number);
-    //pick a new random number
-    
-    //notify the player that a new game is happening
-    $("#response_div").text("");
 }
 
+function reset_game(){
+    the_number = pick_number();
+    $("#response_div").text("");
+}
 
 function make_guess(){
    
     var the_guess = $("#guess_input").val();
-    console.log('value', the_guess, score);
     if( the_guess === '' || isNaN(the_guess)){
         $("#response_div").text("Oops!! please enter a valid number");
     } else{
@@ -56,16 +46,14 @@ function make_guess(){
     $("#startAgain").show();
 }
 
-
  
  function getHighScores(){   
-    console.log(name, score);
+    
     $.ajax({
         url: 'http://localhost:3001/scores',
         method: 'get',
         dataType: 'json',
         success: function( response ){
-            console.log('getscore',response);
 
             const sortedData = response.sort((a,b) => parseInt(a.score) - parseInt(b.score)) ;
             
